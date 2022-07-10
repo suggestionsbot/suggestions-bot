64f461f6ecc33eaf2face72c5d6325b1bc2005b3
skelmis <ethan@koldfusion.xyz>
2022-07-11 00:26:11 +1200

    doc: Up version

--- suggestions/bot.py
+++ suggestions/bot.py
@@ -31,7 +31,7 @@ log = logging.getLogger(__name__)
 
 class SuggestionsBot(commands.AutoShardedInteractionBot, BotBase):
     def __init__(self, *args, **kwargs):
-        self.version: str = "Beta Release 1.0.7"
+        self.version: str = "Beta Release 1.0.8"
         self.main_guild_id: int = 601219766258106399
         self.beta_role_id: int = 995588041991274547
         self.is_prod: bool = True if os.environ.get("PROD", None) else False
