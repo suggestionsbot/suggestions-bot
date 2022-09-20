e74c84cd59e36b6bea7d2ce2bff75fcaff95dc81
skelmis <ethan@koldfusion.xyz>
2022-09-20 22:40:54 +1200

    feat: add count.py because LOC is cool

---
+++ count.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from typing import Union, List
+
+
+def count_lines(directory: Union[str, List[str]]):
+    """Count Python LOC in a given directory."""
+    if isinstance(directory, str):
+        directories = [directory]
+    else:
+        directories = directory
+
+    total_lines: int = 0
+    print("{:>10} |{:>10} | {:<20}".format("ADDED", "TOTAL", "FILE"))
+    print("{:->11}|{:->11}|{:->20}".format("", "", ""))
+    for directory in directories:
+        for path in Path(directory).rglob("*.py"):
+            with open(path.absolute(), "r") as f:
+                newlines = f.readlines()
+                newlines = len(newlines)
+                total_lines += newlines
+                print(
+                    "{:>10} |{:>10} | {:<20}".format(newlines, total_lines, str(path))
+                )
+
+
+count_lines(["./suggestions", "./tests"])
