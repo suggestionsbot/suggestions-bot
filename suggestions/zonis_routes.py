6925940eb15824485aada8294e0f724b993d9cc2
skelmis <ethan@koldfusion.xyz>
2023-01-24 21:14:14 +1300

    feat: add new premium zonis routes

--- suggestions/zonis_routes.py
+++ suggestions/zonis_routes.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 import math
 import os
+from datetime import timedelta
 from typing import TYPE_CHECKING
 
 import disnake
+from bot_base import NonExistentEntry
+from bot_base.caches import TimedCache
 from zonis import client
 
 from suggestions.scheduler import exception_aware_scheduler
@@ -28,7 +31,15 @@ class ZonisRoutes:
             override_key=os.environ.get("ZONIS_OVERRIDE_KEY"),
         )
         self.client.register_class_instance_for_routes(
-            self, "guild_count", "cluster_status", "share_with_devs"
+            self,
+            "guild_count",
+            "cluster_status",
+            "share_with_devs",
+            "refresh_premium",
+            "shared_guilds",
+        )
+        self._shared_guilds_cache: TimedCache = TimedCache(
+            global_ttl=timedelta(minutes=15), lazy_eviction=False
         )
 
     async def start(self):
@@ -71,3 +82,32 @@ class ZonisRoutes:
         )
         embed.set_footer(text=f"Sender: {sender}")
         await channel.send(embed=embed)
+
+    @client.route()
+    async def refresh_premium(self, user_id: int):
+        # TODO Implement the ability to refresh premium states for a user
+        return True
+
+    @client.route()
+    async def shared_guilds(self, user_id: int, guild_ids: list[int]):
+        to_return = []
+        to_check = [gid for gid in guild_ids if gid in self.bot.guild_ids]
+        for guild_id in to_check:
+            key = f"{user_id}|{guild_id}"
+            try:
+                self._shared_guilds_cache.get_entry(key)
+            except NonExistentEntry:
+                pass
+            else:
+                to_return.append(guild_id)
+                continue
+
+            try:
+                await self.bot.http.get_member(guild_id, user_id)
+            except disnake.HTTPException:
+                continue
+            else:
+                to_return.append(guild_id)
+                self._shared_guilds_cache.add_entry(key, True, override=True)
+
+        return to_return
