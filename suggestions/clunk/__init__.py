806ba53c6a7bb8b63225d19a0f8e6f5ce3909f4c
Ethan <47520067+Skelmis@users.noreply.github.com>
2024-02-04 21:58:29 +1300

    chore: add TTL from last access to paginators (#74)
    * chore: add TTL from last access to paginators
    
    * chore: migrate all TimedCache usages to python commons
    
    * feat: add ttl from last access to a few caches

--- suggestions/clunk/__init__.py
+++
@@ -1,3 +0,0 @@
-from .lock import ClunkLock
-from .cache import ClunkCache
-from .clunk import Clunk
--- suggestions/state.py
+++ suggestions/state.py
@@ -14,8 +14,7 @@ from alaric.comparison import EQ
 from alaric.logical import AND
 from alaric.meta import Negate
 from alaric.projections import PROJECTION, SHOW
-from bot_base import NonExistentEntry
-from bot_base.caches import TimedCache
+from commons.caching import NonExistentEntry, TimedCache
 
 from suggestions.objects import GuildConfig, UserConfig
 
@@ -42,17 +41,25 @@ class State:
         )
         self.guild_cache_ttl: timedelta = timedelta(minutes=15)
         self.guild_cache: TimedCache[int, disnake.Guild] = TimedCache(
-            global_ttl=self.guild_cache_ttl, lazy_eviction=False
+            global_ttl=self.guild_cache_ttl,
+            lazy_eviction=False,
+            ttl_from_last_access=True,
         )
         self.view_voters_cache: TimedCache[int, list[str]] = TimedCache(
-            global_ttl=self.autocomplete_cache_ttl, lazy_eviction=False
+            global_ttl=self.autocomplete_cache_ttl,
+            lazy_eviction=False,
+            ttl_from_last_access=True,
         )
 
         self.guild_configs: TimedCache = TimedCache(
-            global_ttl=timedelta(minutes=30), lazy_eviction=False
+            global_ttl=timedelta(minutes=30),
+            lazy_eviction=False,
+            ttl_from_last_access=True,
         )
         self.user_configs: TimedCache = TimedCache(
-            global_ttl=timedelta(minutes=30), lazy_eviction=False
+            global_ttl=timedelta(minutes=30),
+            lazy_eviction=False,
+            ttl_from_last_access=True,
         )
 
         self.existing_error_ids: Set[str] = set()
