bd518336c093cf8c412e8f8aa8eb624e0446c3cb
skelmis <skelmis.craft@gmail.com>
2025-02-17 19:56:51 +1300

    feat: version 3.30

--- suggestions/bot.py
+++ suggestions/bot.py
@@ -58,7 +58,7 @@ logger = Logger(__name__)
 
 class SuggestionsBot(commands.AutoShardedInteractionBot):
     def __init__(self, *args, **kwargs):
-        self.version: str = "Public Release 3.29"
+        self.version: str = "Public Release 3.30"
         self.main_guild_id: int = 601219766258106399
         self.legacy_beta_role_id: int = 995588041991274547
         self.automated_beta_role_id: int = 998173237282361425
